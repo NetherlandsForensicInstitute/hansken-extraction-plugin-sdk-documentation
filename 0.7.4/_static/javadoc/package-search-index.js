c30b1f46aca69f10d14836851d258435e0e266ef
remco <remco@holmes.nl>
2024-05-15 20:33:04 +0200

    0.7.4

---
+++ 0.7.4/_static/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.hansken.plugin.extraction.api"},{"l":"org.hansken.plugin.extraction.api.transformations"},{"l":"org.hansken.plugin.extraction.util"}];updateSearchResults();
